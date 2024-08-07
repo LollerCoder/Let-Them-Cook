822dd77500ae0e94653cdaf443e84c05fb273818
SunnDunn <Atasha@LAPTOP-VEPF9FQO>
2024-08-07 11:00:24 +0800

    Inventory and Notif System

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance { get; private set; }
+
+    [SerializeField] private Dictionary<EIngredientType, int> Inventory = new Dictionary<EIngredientType, int>();
+
+    public void OnAddToInventory(EIngredientType ingredient)
+    {
+        Debug.Log(ingredient + " enter inventory manager");
+
+        if (Inventory.ContainsKey(ingredient))
+        {
+            Inventory[ingredient]++;
+        }
+        else
+        {
+            Debug.Log(ingredient + " Added");
+            Inventory.Add(ingredient, 1);
+        }
+    }
+
+    public int getItemAmount(EIngredientType type)
+    {
+        int amount = 0;
+
+        if (Inventory.ContainsKey(type))
+        {
+            amount = Inventory[type];
+        }
+        else
+        {
+            amount = 0;
+        }
+
+        return amount;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.OnAddItem += OnAddToInventory;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
