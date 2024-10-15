02ac70a4ca5c4f579366140ef71b880b5d3a7090
railey13 <nikosrailey@gmail.com>
2024-10-16 00:08:52 +0800

    Revert "added Shop stuff"
    This reverts commit 73af7c712f0ea923d94980aa65e1c8cb18573460.

--- Assets/Scripts/Database/UnitDatabase/UnitDatabaseManager.cs
+++ Assets/Scripts/Database/UnitDatabase/UnitDatabaseManager.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class UnitDatabaseManager : MonoBehaviour
 {
     [SerializeField] List<Unit> unitList = new List<Unit>();
+    [SerializeField] TMPro.TextMeshPro m_TextMeshPro;
 
     private string filePath = "Assets/Scripts/Database/UnitDatabase/UnitDatabase.txt";
     private StreamWriter writer = null;
@@ -21,20 +22,10 @@ public class UnitDatabaseManager : MonoBehaviour
 
     public static UnitDatabaseManager Instance { get; private set; }
 
-
-    public void addDatabase(string item)
-    {
-        using (var writer = new StreamWriter(filePath, true))
-        {
-            writer.WriteLine(item);
-            writer.Flush();
-        }
-    }
-
     public void saveDatabase()
     {
         //for loops of the list of allies
-        writer = new StreamWriter(filePath, true);
+        writer = new StreamWriter(filePath, false);
         /*writer.WriteLine("CARROT");
         writer.Flush();*/
     }
@@ -66,13 +57,6 @@ public class UnitDatabaseManager : MonoBehaviour
         }
     }
 
-    private void lineChanger(string newText, string fileName, int line_to_edit)
-    {
-        string[] arrLine = File.ReadAllLines(fileName);
-        arrLine[line_to_edit - 1] = newText;
-        File.WriteAllLines(fileName, arrLine);
-    }
-
     private void Awake()
     {
         if (Instance == null)
@@ -89,7 +73,7 @@ public class UnitDatabaseManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //loadDatabase();
+        loadDatabase();
     }
 
     // Update is called once per frame
