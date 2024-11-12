d2cd1382a89160788dcdc21f0dde950b2b4ba400
railey13 <147615890+railey13@users.noreply.github.com>
2024-11-13 04:50:51 +0800

    makes text animation skippable

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -9,9 +9,17 @@ public class DialogueManager : MonoBehaviour {
 
     [SerializeField] private Queue sentences = new Queue();
 
+    public float TextSpeed;
+
+    public Animator animator;
     public Text nameText;
     public Text dialogeText;
+
+    private bool isTyping = false;
+    private bool skipTyping = false;
+
     public void StartDialogue(Dialogue dialogue) {
+        this.animator.SetBool("Open", true);
 
         this.nameText.text = dialogue.name;
 
@@ -25,22 +33,43 @@ public class DialogueManager : MonoBehaviour {
     }
 
     public void DisplayNextSentence() {
-        if(this.sentences.Count == 0) {
+
+        if (this.isTyping) { // check if its currently typing before skipping it
+            this.skipTyping = true;
+            return;
+        }
+
+        if (this.sentences.Count == 0) {
             this.EndDialogue();
             return;
         }
 
         string sentence = (string)this.sentences.Dequeue();
-        this.dialogeText.text = sentence;
+        this.StopAllCoroutines();
+        this.StartCoroutine(this.TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence) {
+        this.dialogeText.text = "";
+        this.isTyping = true; // make isTyping true since its already here
+        this.skipTyping = false; // make it false at the start
+        foreach (char letter in sentence.ToCharArray()) {
+            if (this.skipTyping) {
+                this.dialogeText.text = sentence;
+                break;
+            }
+            this.dialogeText.text += letter;
+
+            yield return new WaitForSeconds(this.TextSpeed);
+        }
+
+        this.isTyping = false; // reset after done typing
     }
 
     public void EndDialogue() {
-        Debug.Log("End");
+        this.animator.SetBool("Open", false);
     }
-
-    void Start() {
-        
-    }
+    
     public void Awake() {
         if (Instance == null) {
             Instance = this;
@@ -49,5 +78,4 @@ public class DialogueManager : MonoBehaviour {
             Destroy(this.gameObject);
         }
     }
-
 }
