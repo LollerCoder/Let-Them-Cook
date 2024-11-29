c25febf82c36edd54403d1bec3215748f1f087b5
LollerCoder <jairiojaps@gmail.com>
2024-11-30 05:18:54 +0800

    Tutorial Dialogue and triggers set
    Fully complete tutorial

--- Assets/Scripts/Dialogue/Tutorial/MoveToCorrectPosition/NextToJenkins.cs
+++ Assets/Scripts/Dialogue/Tutorial/MoveToCorrectPosition/NextToJenkins.cs
@@ -25,6 +25,8 @@ public class NextToJenkins : MonoBehaviour
         {
             playerEntered.Invoke();
             destroyPos.Invoke();
+            
+            
         }
     }
 }
--- Assets/Scripts/Dialogue/Tutorial/TutorialDialogue.cs
+++ Assets/Scripts/Dialogue/Tutorial/TutorialDialogue.cs
@@ -6,8 +6,19 @@ public class DialogueStart : MonoBehaviour
 {
     public static DialogueStart Instance = null;
     [SerializeField] private DialogueTrigger start;
+
     [SerializeField] private DialogueTrigger nextToJenkins;
     [SerializeField] private DialogueTrigger notNextToJenkins;
+    [SerializeField] private DialogueTrigger attackedJenkins;
+
+    
+    [SerializeField] private DialogueTrigger nextToFlower;
+    [SerializeField] private DialogueTrigger notNextToFlower;
+
+    [SerializeField] private DialogueTrigger outlines;
+    [SerializeField] private DialogueTrigger turnOrder;
+
+    private int turnsCounted = 0;
 
 
     //bool startDone = false;
@@ -15,7 +26,10 @@ public class DialogueStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        BattleUI.Instance.HideWaitButton();
         DialogueManager.Instance.StartDialogue(this.start.dialogue);
+        //EventBroadcaster.Instance.AddObserver(EventNames.BattleUI_Events.WAIT_BUTTON_SHOW, this.damagedJenkins);
+        EventBroadcaster.Instance.AddObserver(EventNames.BattleManager_Events.NEXT_TURN, this.turnTrack);
     }
 
     public void jenkins()
@@ -27,12 +41,60 @@ public class DialogueStart : MonoBehaviour
         DialogueManager.Instance.StartDialogue(this.notNextToJenkins.dialogue);
     }
 
+    public void damagedJenkins()
+    {
+        DialogueManager.Instance.StartDialogue(this.attackedJenkins.dialogue);
+    }
+
+    public void flower()
+    {
+        DialogueManager.Instance.StartDialogue(this.nextToFlower.dialogue);
+    }
+
+    public void notFlower()
+    {
+        DialogueManager.Instance.StartDialogue(this.notNextToFlower.dialogue);
+    }
+
+    public void outlinePlay()
+    {
+        DialogueManager.Instance.StartDialogue(this.outlines.dialogue);
+    }
+
+    public void turnOrderPlay()
+    {
+        DialogueManager.Instance.StartDialogue(this.turnOrder.dialogue);
+    }
+
+    private void turnTrack()
+    {
+        switch (turnsCounted)
+        {
+            case 0:
+                
+                this.damagedJenkins();
+                break;
+            case 1:
+                this.outlinePlay();
+                break;
+            case 2:
+                this.turnOrderPlay();
+                break;
+
+        }
+        turnsCounted++;
+
+        Debug.Log("Turn: " + turnsCounted);
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-           
+        if (turnsCounted > 0 && UnitActionManager.Instance.OnMove == true)
+        {
+            BattleUI.Instance.ShowWaitButton();
+        }
     }
 
     public void Awake()
