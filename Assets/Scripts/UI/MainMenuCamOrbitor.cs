b82ce15f60c05ae6c322255a83bc064e77c1470c
KenshinRR <krreblando15@gmail.com>
2025-01-23 14:54:49 +0800

    Main Menu V2
    Main Menu is now similar to Minecraft's main menu but instead of panoramic, the camera rotates around a random level

---
+++ Assets/Scripts/UI/MainMenuCamOrbitor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class MainMenuCamOrbitor : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] List<GameObject> levelMapPrefabs = new List<GameObject>();
+
+    private GameObject _generatedLevel;
+
+    private Vector3 _center;
+
+    private void OnEnable()
+    {
+        int levelToGenerate = Random.Range(0, levelMapPrefabs.Count - 1);
+        this._generatedLevel = GameObject.Instantiate(levelMapPrefabs[levelToGenerate],this.gameObject.transform);
+
+        Vector3 sumVector = new Vector3(0f, 0f, 0f);
+
+        foreach (Transform child in _generatedLevel.transform)
+        {
+            sumVector += child.position;
+        }
+
+        this._center = sumVector / _generatedLevel.transform.childCount;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.transform.RotateAround(this._center, Vector3.up, 20 * Time.deltaTime);
+        cam.transform.LookAt(this._center);
+    }
+}
