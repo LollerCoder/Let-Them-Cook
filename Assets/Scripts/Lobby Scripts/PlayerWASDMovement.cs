8bd02fe11567e361f6d86cfabe26d982d77d2db9
KenshinRR <krreblando15@gmail.com>
2025-05-07 18:28:51 +0800

    Player movement

---
+++ Assets/Scripts/Lobby Scripts/PlayerWASDMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerWASDMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float runSpeed = 10f;
+
+    [Header("References")]
+    Animator myAnimator;
+    SpriteRenderer mySpriteRenderer;
+    Vector2 moveInput;
+    Rigidbody myrigidbody;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myrigidbody = GetComponent<Rigidbody>();
+
+        //animations
+        myAnimator.SetBool("Turn", true);
+        myAnimator.SetBool("Ally", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+    }
+
+    private Vector2 GetInput()
+    {
+        Vector2 inputs = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputs += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputs += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputs += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputs += Vector2.down;
+        }
+
+        return inputs;
+    }
+
+    private void Walk()
+    {
+        moveInput = GetInput();
+
+        //moving
+        Vector3 PlayerVelocity = new Vector3(moveInput.x * runSpeed, 0, moveInput.y * runSpeed);
+        myrigidbody.velocity = PlayerVelocity;
+
+        //flip
+        if (moveInput.x == -1) mySpriteRenderer.flipX = true;
+        if (moveInput.x == 1) mySpriteRenderer.flipX = false;
+
+        //animating
+        if (moveInput != Vector2.zero) myAnimator.SetBool("Walk", true);
+        else myAnimator.SetBool("Walk", false);
+    }
+}
