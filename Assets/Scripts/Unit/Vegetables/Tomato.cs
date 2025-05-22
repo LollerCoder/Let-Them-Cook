235b34657249dd26a7ac9e5ee790eee291baaef0
KenshinRR <krreblando15@gmail.com>
2025-05-22 12:26:47 +0800

    Removed Redundant code from Tomato

--- Assets/Scripts/Unit/Vegetables/Tomato.cs
+++ Assets/Scripts/Unit/Vegetables/Tomato.cs
@@ -5,102 +5,9 @@ using UnityEngine;
 public class Tomato : Unit
 {
 
-    private void HpBarShow(Parameters param)
-    {
-        bool isYou = false;
-        Unit unit = param.GetUnitExtra(UNIT);
-
-
-        //if (this.Type == EUnitType.Enemy)//enemy
-        //{
-        //    this.hpBar.transform.Find("Slider").GetComponentInChildren<Image>().color = new Color(0.8941177f, 0, 0.05098039f, 1);
-
-        //}
-
-        //if (this.Type == EUnitType.Ally)//ally
-        //{
-        //    this.hpBar.transform.Find("Slider").GetComponentInChildren<Image>().color = new Color(0.0619223f, 0.2870282f, 0.8415094f, 1);
-        //}
-
-        if (UnitActionManager.Instance.UnitOrder[0] == this && this.Type != EUnitType.Enemy)//its you
-        {
-            isYou = true;
-        }
-        this.hpBar.GetComponent<HpBar>().setColor(unit.Type, isYou);
-
-        if (unit == this)
-        {
-            this.hpBar.GetComponentInChildren<HpBar>().hpPopUp(this.hpBar, this.maxhp, this.hp);
-        }
-    }
-
-    private void HpBarHide(Parameters param)
-    {
-        Unit unit = param.GetUnitExtra(UNIT);
-
-
-
-        if (unit == this)
-        {
-            this.hpBar.GetComponentInChildren<HpBar>().hpHide(this.hpBar);
-        }
-
-    }
-
-    private void BuffArrowShow(Parameters param)
-    {
-        if (this == param.GetUnitExtra("UNIT"))
-        {
-            this.BufController.SetBool("isBuffed", true);
-            //Debug.Log("Buffed");
-        }
-
-    }
-
-    private void DebuffArrowShow(Parameters param)
-    {
-        if (this == param.GetUnitExtra("UNIT"))
-        {
-            this.DebufController.SetBool("isDebuffed", true);
-            // Debug.Log("Debuffed");
-        }
-    }
-
-    private void BuffArrowHide(Parameters param)
-    {
-        if (this == param.GetUnitExtra("UNIT"))
-        {
-            this.BufController.SetBool("isBuffed", false);
-            //Debug.Log("buffGone");
-        }
-
-    }
-
-    private void DebuffArrowHide(Parameters param)
-    {
-        if (this == param.GetUnitExtra("UNIT"))
-        {
-            this.DebufController.SetBool("isDebuffed", false);
-            // Debug.Log("DebuffedGone");
-        }
-    }
-
-    [SerializeField]
-    Animator BufController;
-
-    [SerializeField]
-    Animator DebufController;
-
     protected override void Start()
     {
 
-        EventBroadcaster.Instance.AddObserver(EventNames.BattleUI_Events.HIDE_HP, this.HpBarHide);
-        EventBroadcaster.Instance.AddObserver(EventNames.BattleUI_Events.SHOW_HP, this.HpBarShow);
-
-        EventBroadcaster.Instance.AddObserver(EventNames.BattleUI_Events.DEBUFF_SHOW, this.DebuffArrowShow);
-        EventBroadcaster.Instance.AddObserver(EventNames.BattleUI_Events.BUFF_SHOW, this.BuffArrowShow);
-        EventBroadcaster.Instance.AddObserver(EventNames.BattleUI_Events.DEBUFF_HIDE, this.DebuffArrowHide);
-        EventBroadcaster.Instance.AddObserver(EventNames.BattleUI_Events.BUFF_SHOW, this.BuffArrowHide);
 
         this.animator = this.GetComponent<Animator>();
 
@@ -113,6 +20,7 @@ public class Tomato : Unit
         this.hp = this.maxhp;
         this.atk = 4;
         this.def = 1;
+
 
         //UnitActionManager.Instance.StoreUnit(this);
         //this.charName = "Carrot";
