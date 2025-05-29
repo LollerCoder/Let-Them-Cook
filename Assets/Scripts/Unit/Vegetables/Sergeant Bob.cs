ce95f5f80ea5556037cf819a117c2b2dbcbda6b8
KenshinRR <krreblando15@gmail.com>
2025-05-29 14:53:55 +0800

    Added Sergeant Bob
    TODO:
    Fix Shove
    Add the Yell skill

---
+++ Assets/Scripts/Unit/Vegetables/Sergeant Bob.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SergeantBob : Unit
+{
+    protected override void Start()
+    {
+
+
+        this.skillList.Add("Shove");
+        this.skillList.Add("Yell");
+
+
+        this.acc = 10;
+        this.spd = 2;
+        this.maxhp = 25;
+        this.hp = this.maxhp;
+        this.atk = 2;
+        this.def = 4;
+
+        base.Start();
+    }
+}
