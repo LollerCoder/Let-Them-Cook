96e2f8b6f44c837d85122a9fb3231c515b02750d
LollerCoder <jairiojaps@gmail.com>
2025-07-09 06:24:39 +0800

    Objectives and Endscreen WIP

--- Assets/Scripts/Level Objectives/Objective copiier.cs
+++ Assets/Scripts/Level Objectives/Objective copiier.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class Objectivecopiier : MonoBehaviour
 {
     [SerializeField] private GameObject toCopy;
-    [SerializeField] private Transform copyTo;
+    [SerializeField] private GameObject copyTo;
+
+
     // Start is called before the first frame update
     void Start()
     {
+       
         EventBroadcaster.Instance.AddObserver(EventNames.BattleManager_Events.CHECK_END_CONDITION, CreateObjectiveCopy);
     }
 
     void CreateObjectiveCopy()
     {
-        GameObject clone = Instantiate(toCopy, copyTo);
+        RectTransform toCopyRT = toCopy.GetComponent<RectTransform>();
+        RectTransform copyToRT = copyTo.GetComponent<RectTransform>();
+
+        toCopyRT.SetParent(copyToRT,false);
+
+        toCopyRT.anchoredPosition = new  Vector2(0, -120);
+
+        
     }
     // Update is called once per frame
     void Update()
