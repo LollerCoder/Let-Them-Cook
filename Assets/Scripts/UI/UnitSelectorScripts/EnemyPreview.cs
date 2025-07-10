a0e3f14e4221075d2f5cd29bc26d8af1f006968d
shrimp-n00dle <vingno.elizabeth@gmail.com>
2025-07-10 15:05:46 +0800

    Level 2 Enemies added

---
+++ Assets/Scripts/UI/UnitSelectorScripts/EnemyPreview.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyPreview : MonoBehaviour
+{
+    //[SerializeField] GameObject enemyList;
+
+ //   private List<SpriteRenderer> enemyList = new List<SpriteRenderer>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        // //get the image in each child gameobject and post it on the content
+      //  for (int i = 0; i < enemyList.Count; i++)
+        //{
+            //Instatiate a Image
+           // Image enemyImg = new Image();
+            //assign the sprite to the image
+            //enemyImg = enemyList[i].GetComponent<Sprite>().GetSpriteID();
+            
+            //add image to content
+         //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
