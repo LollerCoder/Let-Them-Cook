2b22da6258792b4e5897abe6478f3288e7eafe3e
KenshinRR <krreblando15@gmail.com>
2025-07-15 19:03:39 +0800

    Added a second wave to Level 3

--- Assets/Scripts/Level Objectives/Level3Objective.cs
+++ Assets/Scripts/Level Objectives/Level3Objective.cs
@@ -14,11 +14,18 @@ public class Level3Objective : MonoBehaviour
     [SerializeField]
     private List<Tile> spawnTile = new List<Tile>();
 
+    [Header("Wave Spawn Enemy Data")]
+    [SerializeField]
+    private List<GameObject> enemyObjs_wave2 = new List<GameObject>();
+    [SerializeField]
+    private List<Tile> spawnTile_wave2 = new List<Tile>();
+
     // Start is called before the first frame update
     void Start()
     {
         EventBroadcaster.Instance.AddObserver(EventNames.Level3_Objectives.WOKE_UP, this.SpawnEnemies);
         EventBroadcaster.Instance.AddObserver(EventNames.Level3_Objectives.KEY_FOUND, this.GiveAllyKey);
+        EventBroadcaster.Instance.AddObserver(EventNames.Level3_Objectives.KEY_FOUND, this.SpawnWave2Enemies);
         EventBroadcaster.Instance.AddObserver(EventNames.BattleManager_Events.ON_START, this.LateStart);
         
     }
@@ -55,6 +62,19 @@ public class Level3Objective : MonoBehaviour
         }
 
         EventBroadcaster.Instance.PostEvent(EventNames.EnemySpawn_Events.SPAWN_ENEMY);
+    }
+
+    public void SpawnWave2Enemies(Parameters param)
+    {
+        for (int i = 0; i < enemyObjs_wave2.Count; i++)
+        {
+            UnitManager.Instance.addUnit("Enemy",
+                enemyObjs_wave2[i],
+                spawnTile_wave2[i],
+                EUnitType.Enemy,
+                new List<Effect>()
+                );
+        }
     }
 
     public void GiveAllyKey(Parameters param)
