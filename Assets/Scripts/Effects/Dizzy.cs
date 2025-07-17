8e3f432c41f1ff9aa504e74c095bd5cd0a684226
railey13 <147615890+railey13@users.noreply.github.com>
2025-07-18 01:19:00 +0800

    Update Dizzy.cs

--- Assets/Scripts/Effects/Dizzy.cs
+++ Assets/Scripts/Effects/Dizzy.cs
@@ -1,22 +1,29 @@
+using UnityEngine;
 
 public class Dizzy : Effect
 {
     public Dizzy(int _duration, Unit _effectMaker) : base(_duration, _effectMaker)
     {
         this.EffectName = "Dizzy";
+        _effectMaker.ToggleDebuffArrow(true);
     }
 
     public override void EffectAction(Unit unitAffected)
     {
         PopUpManager.Instance.addPopUp("Dizzy!", unitAffected.transform);
-        UnitActionManager.Instance.OnAttack = false;
-        UnitActionManager.Instance.OnMove = false;
-        UnitActionManager.Instance.hadAttacked = true;
-        UnitActionManager.Instance.hadMoved = true;
+        UnitActionManager.Instance.effectSkipTurn = true;
+        //UnitActionManager.Instance.OnAttack = false;
+        //UnitActionManager.Instance.OnMove = false;
+        //UnitActionManager.Instance.hadAttacked = true;
+        //UnitActionManager.Instance.hadMoved = true;
+        //EventBroadcaster.Instance.PostEvent(EventNames.BattleManager_Events.NEXT_TURN);
     }
 
     public override void EffectAfterAction(Unit unitAffected)
     {
-
+        Debug.Log("DONE");
+    }
+    protected override void HideArrow(Unit unitAffected) {
+        unitAffected.ToggleDebuffArrow(false);
     }
 }
