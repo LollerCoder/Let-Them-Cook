cb23a7ca934073d851aeb866445a8a309d36d0da
KenshinRR <krreblando15@gmail.com>
2025-07-18 14:48:23 +0800

    Added cutscene before level 4

---
+++ Assets/Scripts/Dialogue/CutScenes/GeneralCutSceneDialogue.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GeneralCutSceneDialogue : MonoBehaviour
+{
+    [Header("Dialogues")]
+    [SerializeField] private List<Dialogue> _Dialogues = new List<Dialogue>();
+    private int _CurrentDialogueIndex = 0;
+    [SerializeField] private bool _StartDialogueOnStart;
+
+    [Header("Scene")]
+    [SerializeField]
+    private string _NextSceneToLoad;
+
+    private void Start()
+    {
+        EventBroadcaster.Instance.AddObserver(EventNames.Dialogue_Events.ON_DIALOGUE_FINISHED, this.LoadNextScene);
+
+        if (_StartDialogueOnStart) this.StartDialogue();
+    }
+
+    public void StartDialogue()
+    {
+        DialogueManager.Instance.StartDialogue(this._Dialogues[this._CurrentDialogueIndex]);
+        this._CurrentDialogueIndex++;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(this._NextSceneToLoad);
+    }
+}
